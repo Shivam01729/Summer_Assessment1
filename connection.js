aac88dfb230dd4aed3364a2d56da1073834ed7fd
Shivam Raj <119311686+Shivam01729@users.noreply.github.com>
2025-07-06 14:39:12 +0530

    sec

--- connection.js
+++ connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const url =
-  "mongodb+srv://krishanmohan1266:C7La6gbsUGZEDQjR@cluster0.eus39lx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://shivamsingh97719:DSksyReRiaLXQeQG@cluster0.a8xoxhg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+";
 
 const connection = async () => {
   return mongoose.connect(url);
